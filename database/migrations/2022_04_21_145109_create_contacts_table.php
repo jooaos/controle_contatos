5bdcd53f360b145393f01f285c5a7a0fbb6537d6
João Victor Silva Soares <jovictors.soares@gmail.com>
2022-04-21 11:01:34 -0400

    ajuste na migration para a tabela contact

--- database/migrations/2022_04_21_145109_create_contacts_table.php
+++ database/migrations/2022_04_21_145109_create_contacts_table.php
@@ -16,7 +16,7 @@ class CreateContactsTable extends Migration
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('contact', 9);
+            $table->string('contact');
             $table->string('email');
             $table->softDeletes();
             $table->timestamps();
