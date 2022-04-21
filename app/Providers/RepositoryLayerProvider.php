e962f1249c02b2649041d25e6364b10a06581157
João Victor Silva Soares <jovictors.soares@gmail.com>
2022-04-21 11:40:02 -0400

    criacao do RepositoryLayerProvider

---
+++ app/Providers/RepositoryLayerProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\Contact\ContactRepository;
+use App\Repositories\Contact\Contract\ContactRepositoryContract;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryLayerProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
