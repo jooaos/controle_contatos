a5efaee108d4bfed589e4e7bc5718b6b0dbe6bc2
João Victor Silva Soares <jovictors.soares@gmail.com>
2022-04-21 14:42:10 -0400

    criacao do ContactController e rotas

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Contact\Contract\ContactServiceContract;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * @var ContactServiceContract $contactService
+     */
+    protected ContactServiceContract $contactService;
+
+    /**
+     * @param ContactServiceContract $contactService
+     */
+    public function __construct(ContactServiceContract $contactService)
+    {
+        $this->contactService = $contactService;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return $this->contactService->getAll();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return $this->contactService->create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return $this->contactService->getById($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return $this->contactService->update($id, $request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return $this->contactService->delete($id);
+    }
+}
