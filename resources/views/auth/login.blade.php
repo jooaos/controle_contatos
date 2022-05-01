ac6307473c68519da1225b56013d66b6f6214685
João Victor Silva Soares <jovictors.soares@gmail.com>
2022-05-01 10:23:30 -0400

    criacao das blades de login e logout

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,33 @@
+@extends('template.template')
+
+@section('title', 'Login')
+
+@section('content')
+    <section>
+        <div class="d-flex justify-content-between align-items-center">
+            <div class="mt-5">
+                <h3 class="mt-4">Login</h3>
+            </div>
+            <div>
+                <a href="{{ route('contacts.index') }}" type="button" class="btn btn-info text-white">
+                    Home
+                </a>
+            </div>
+        </div>
+        <form action="{{ route('login') }}" method="POST" class="mt-4">
+            @csrf
+            <div class="mb-3">
+                <label for="email" class="form-label">E-mail<span>*</span></label>
+                <input type="email" name="email" class="form-control" id="email" required>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">E-mail<span>*</span></label>
+                <input type="password" name="password" class="form-control" id=" password" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </form>
+        @if ($errors->has('login-error'))
+            <div class="mt-3 text-danger"> <h6 class="p-0">{{ $errors->first('login-error') }}</h6></div>
+        @endif
+    </section>
+@endsection
