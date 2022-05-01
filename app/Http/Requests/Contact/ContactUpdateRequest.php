3f2298df6db9e31b6b1e6ef89b944f3b03585246
João Victor Silva Soares <jovictors.soares@gmail.com>
2022-05-01 09:28:54 -0400

    ajuste na request de update de contato

--- app/Http/Requests/Contact/ContactUpdateRequest.php
+++ app/Http/Requests/Contact/ContactUpdateRequest.php
@@ -24,10 +24,11 @@ class ContactUpdateRequest extends FormRequest
      */
     public function rules()
     {
+        $contact = $this->route('contact');
         return [
             'name' => 'required|string|min:5',
-            'email' => ['required', 'email', Rule::unique('contacts', 'email')->ignore($this->contact)->whereNull('deleted_at')],
-            'contact' => ['required', 'string', 'min:9', Rule::unique('contacts', 'contact')->ignore($this->contact)->whereNull('deleted_at')],
+            'email' => ['required', 'email', Rule::unique('contacts', 'email')->ignore($contact)->whereNull('deleted_at')],
+            'contact' => ['required', 'string', 'min:9', Rule::unique('contacts', 'contact')->ignore($contact)->whereNull('deleted_at')],
         ];
     }
 }
